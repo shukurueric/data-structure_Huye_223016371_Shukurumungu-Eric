86cffbe88db702055c24dc5fb5c503ece7613863
shukurueric <shukurumungueric1@gmail.com>
2025-02-21 11:15:10 +0200

    Add files via upload

---
+++ ASSIGNMENT OF OOP/Calculating operations code and creenshot/Calculating operations code and creenshot.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a,b;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b";
+    cin>>b;
+cout << "Sum: " << a + b << endl;
+cout << "Difference: " << a - b << endl;
+cout << "Product: " << a * b << endl;
+cout << "Quotient: " << a / b << endl;
+cout << "Remainder: " << a % b << endl;
+
+}
+
---
+++ ASSIGNMENT OF OOP/Variables declaration code and creenshot/Variable declaration code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20; 
+    double price = 19.99;
+    char grade = 'A'; 
+    string name = "Alice"; 
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Price: $" << price << endl;
+    cout << "Grade: " << grade << endl;
+    
+    return 0;
+}
+
